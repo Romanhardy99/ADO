351a4671714c4c6604d79c302116758bcf5fa30a
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-03-31 21:46:08 +0400

    Tables aligment DONE

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -16,10 +16,15 @@ namespace ADO
             Console.WriteLine(connection_string);
             connection = new SqlConnection(connection_string);
 
-            string cmd = "SELECT * FROM Directors";
+            string cmd = "SELECT last_name,first_name FROM Directors";
             Select(cmd);
+
             Console.WriteLine($"Количество записей: {Scalar("SELECT COUNT (*) FROM Directors")}");
-            Select("SELECT * FROM Movies");
+            Select("title,release_date,last_name,first_name", "Movies,Directors", "director=director_id");
+            /*Select
+                (
+                    "SELECT title,release_date,first_name,last_name FROM Movies, Directors WHERE director=director_id"
+                );*/
         }
 
         static void Select(string cmd)
@@ -27,17 +32,50 @@ namespace ADO
             SqlCommand command = new SqlCommand(cmd, connection);
             connection.Open();
             SqlDataReader reader = command.ExecuteReader();
+            
+            //Определяем максимальный размер данных в каждом поле таблицы:
+            int[] string_sizes = new int[reader.FieldCount]; //Этот массив хранит максимальные размеры строк для каждого поля
+            int interval = 10;
+            //Вычисляем максимальные размеры строк:
+            for(int i =0;i<reader.FieldCount;i++)
+                if (reader.GetName(i).ToString().Length > string_sizes[i]) 
+                    string_sizes[i] = reader.GetName(i).ToString().Length + interval;
+            while(reader.Read())
+            {
+                for(int i = 0; i < reader.FieldCount;i++)
+                {
+                    if (reader[i].ToString().Length > string_sizes[i]) 
+                        string_sizes[i] = reader[i].ToString().Length + interval;
+                }
+            }
+            reader.Close();
+            //Заново выполдняем запрос  и выводим результаты запроса:
+            reader = command.ExecuteReader();
             for (int i = 0; i < reader.FieldCount; i++)
-                Console.Write($"{reader.GetName(i)}\t");
+            {
+                Console.Write($"{reader.GetName(i).PadRight(string_sizes[i])}"); 
+                //if(reader.GetName(i).ToString().Length > string_sizes[i]) string_sizes[i] = reader.GetName(i).ToString().Length + 1;
+            }
             Console.WriteLine();
+            for (int i = 0; i < string_sizes.Sum(); i++) Console.Write("-"); Console.WriteLine();
             while (reader.Read())
             {
-                Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+                //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+                for(int i =0; i < reader.FieldCount;i++)
+                {
+                    Console.Write(reader[i].ToString().PadRight(string_sizes[i]));
+                }
+                Console.WriteLine();
             }
             reader.Close();
             connection.Close();
         }
-
+        static void Select(string fields, string tables, string condition = "")
+        {
+            string cmd = $"SELECT {fields} FROM {tables}";
+            if (condition != "") cmd += $" WHERE {condition}";
+            Select(cmd);
+        }
         static object Scalar(string cmd)
         {
             object value = null;
