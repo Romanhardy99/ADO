3fef0dc71163e212a97432ea2e27cdcbb487376c
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-03-31 22:43:26 +0400

    System.Configuration DONE

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ADO
 {
@@ -12,7 +13,8 @@ namespace ADO
         
         static void Main(string[] args)
         {
-            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+            //string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+            string connection_string = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
             Connector connector = new Connector(connection_string);
 
             connector.Select("SELECT * FROM Directors");
