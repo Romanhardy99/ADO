43b56f3b6f81171b7e6963c742cb016fdd9a2f54
R.Kulemin <R.Kulemin@PC-KULEMIN-RA>
2026-03-31 17:30:14 +0400

    Create class DataBase

---
+++ ADO/DataBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO
+{
+    internal class DataBase
+    {
+        private SqlConnection connection;
+
+        public DataBase(SqlConnection _connection)
+        {
+            this.connection = _connection;
+        }
+
+        public void ExecuteSelect(string cmd)
+        {
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            
+            SqlDataReader reader = command.ExecuteReader();
+
+            for (int i = 0; i < reader.FieldCount; i++)
+                Console.Write($"{reader.GetName(i),-20}\t");
+            Console.WriteLine();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i],-20}\t");
+                }
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+
+        public object ExecuteScalar(string cmd)
+        {
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+
+            object value = command.ExecuteScalar();
+
+            connection.Close();
+            return value;
+        }
+    }
+}
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -9,43 +9,16 @@ namespace ADO
 {
     internal class Program
     {
-        static SqlConnection connection;
         static void Main(string[] args)
         {
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
-            Console.WriteLine(connection_string);
-            connection = new SqlConnection(connection_string);
 
-            string cmd = "SELECT * FROM Directors";
-            Select(cmd);
-            Console.WriteLine($"Количество записей: {Scalar("SELECT COUNT (*) FROM Directors")}");
-            Select("SELECT * FROM Movies");
-        }
+            SqlConnection connection = new SqlConnection(connection_string);
+            DataBase db = new DataBase(connection);
 
-        static void Select(string cmd)
-        {
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            for (int i = 0; i < reader.FieldCount; i++)
-                Console.Write($"{reader.GetName(i)}\t");
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
-            }
-            reader.Close();
-            connection.Close();
-        }
-
-        static object Scalar(string cmd)
-        {
-            object value = null;
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
-            value = command.ExecuteScalar();
-            connection.Close();
-            return value;
+            db.ExecuteSelect("SELECT * FROM Directors");
+            Console.WriteLine($"Количество записей: {db.ExecuteScalar("SELECT COUNT (*) FROM Directors")}");
+            db.ExecuteSelect("SELECT * FROM Movies");     
         }
     }
 }
