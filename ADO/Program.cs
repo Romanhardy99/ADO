8b9cde97177849007028321704c4235602a05946
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-04-02 22:55:21 +0400

    insert in progress

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -10,15 +10,21 @@ namespace ADO
 {
     internal class Program
     {
-        
         static void Main(string[] args)
         {
             //string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
             string connection_string = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
             Connector connector = new Connector(connection_string);
 
+            Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
+            Console.WriteLine(connector.GetNextPrimaryKey("Movies"));
+            connector.Insert($"INSERT Directors(director_id,first_name,last_name)VALUES({connector.GetNextPrimaryKey("Directors")},N'Peter',N'Jackson')");
+
             connector.Select("SELECT * FROM Directors");
-            connector.Select("title,first_name,last_name", "Movies,Directors", "director=director_id");
+            connector.Select("movie_id,title,first_name,last_name", "Movies,Directors", "director=director_id");
+
+            //connector.Insert("INSERT Directors(director_id,first_name,last_name)VALUES(13,N'The',N'Wachowski')");
+            //connector.Select("title,first_name,last_name", "Movies,Directors", "director=director_id");
 
             //Console.WriteLine(connection_string);
             //connection = new SqlConnection(connection_string);
