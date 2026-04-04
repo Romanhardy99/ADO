049c0d5e342c3f0d27df8085625677de6d27e269
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-04-04 22:58:42 +0400

    DBtools DLL DONE

---
+++ DLLcheck/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using DBtools;
+
+namespace DLLcheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Connector connector = new Connector(ConfigurationManager.ConnectionStrings["Movies_PV_522"].ConnectionString);
+            connector.Select("*", "Directors");
+            connector.Select("title,release_date,first_name,last_name","Movies,Directors","director=director_id");
+
+            Connector connectorAcademy = new Connector(ConfigurationManager.ConnectionStrings["PV_522_Import"].ConnectionString);
+            connectorAcademy.Select("*", "Disciplines");
+        }
+    }
+}
