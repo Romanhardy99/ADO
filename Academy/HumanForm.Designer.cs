86a8a91b9746c4d1122d4e30bed04e487e02c027
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-04-16 22:40:44 +0400

    TeacherForm in progress

--- Academy/HumanForm.Designer.cs
+++ Academy/HumanForm.Designer.cs
@@ -195,7 +195,7 @@
             this.buttonBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonBrowse.Location = new System.Drawing.Point(520, 254);
             this.buttonBrowse.Name = "buttonBrowse";
-            this.buttonBrowse.Size = new System.Drawing.Size(89, 45);
+            this.buttonBrowse.Size = new System.Drawing.Size(89, 35);
             this.buttonBrowse.TabIndex = 14;
             this.buttonBrowse.Text = "Обзор";
             this.buttonBrowse.UseVisualStyleBackColor = false;
