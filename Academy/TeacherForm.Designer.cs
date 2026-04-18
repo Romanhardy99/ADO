2f7e21a1bc975986b21bb756be5477743dfd1bd7
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-04-18 20:08:22 +0400

    fix

--- Academy/TeacherForm.Designer.cs
+++ Academy/TeacherForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.dtpWorkSince = new System.Windows.Forms.DateTimePicker();
             this.textBoxRate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dtpWorkSince
@@ -39,7 +41,7 @@
             this.dtpWorkSince.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtpWorkSince.Location = new System.Drawing.Point(190, 254);
             this.dtpWorkSince.Name = "dtpWorkSince";
-            this.dtpWorkSince.Size = new System.Drawing.Size(200, 31);
+            this.dtpWorkSince.Size = new System.Drawing.Size(246, 31);
             this.dtpWorkSince.TabIndex = 18;
             // 
             // textBoxRate
@@ -49,20 +51,46 @@
             this.textBoxRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxRate.Location = new System.Drawing.Point(190, 291);
             this.textBoxRate.Name = "textBoxRate";
-            this.textBoxRate.Size = new System.Drawing.Size(64, 31);
+            this.textBoxRate.Size = new System.Drawing.Size(246, 31);
             this.textBoxRate.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Начало работы:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(70, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Зарплата:";
             // 
             // TeacherForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(697, 422);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxRate);
             this.Controls.Add(this.dtpWorkSince);
             this.Name = "TeacherForm";
             this.Text = "TeacherForm";
             this.Controls.SetChildIndex(this.dtpWorkSince, 0);
             this.Controls.SetChildIndex(this.textBoxRate, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +100,7 @@
 
         private System.Windows.Forms.DateTimePicker dtpWorkSince;
         private System.Windows.Forms.TextBox textBoxRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
